e0ac3b451bc41f3b343b21ed425a9caf6c46a47e
ttamura01git config --global user.email ttamura010164@me.com~git remote add origin https://github.com/ttamura01/git-fed_rate_cut.gitgit push origin mainls <ttamura010164@me.com>
2026-02-16 14:23:48 -0500

    some new information added

--- obbb_tariff_impact.r
+++ obbb_tariff_impact.r
@@ -16,7 +16,7 @@ data_url <- "https://budgetlab.yale.edu/research/combined-distributional-effects
 
 download.file(data_url, "ds.xlsx")
 
-raw <- read_excel("tbl_sep.xlsx", sheet = "F2", range = "A5:E15") %>% 
+raw <- read_excel("tbl_aug.xlsx", sheet = "F2", range = "A5:E15") %>% 
   select(decile = "Income Decile", "avg_income_2025" = `Average Income After Transfers and Taxes (2025 dollars)`, obbb = "OBBBA (via CBO)", tariffs_2025 = "2025 Tariff Increases", net_total = Total)
 
 # Make decile an ordered factor so the x-axis is in the right order
@@ -43,13 +43,13 @@ ggplot(bars, aes(x = decile, y = impact, fill = component)) +
              size = 4, color = "#b30059", alpha = 1, inherit.aes = FALSE) +
   geom_text(data = dat, aes(x = decile, y = net_total,
                             label = dollar(net_total, accuracy = 1)),
-            vjust = 0.5, hjust = -0.25, size = 3.0, color = "#b30059",fontface = "bold", inherit.aes = FALSE) +
+            vjust = 0.5, hjust = -0.25, size = 4.0, color = "#b30059",fontface = "bold", inherit.aes = FALSE) +
   annotate(geom = "label",
            x = 7.00,
            y = 13000,
            label = "Only Top 10% of US households,\na household income of $517,000 or more, \ncould get benefits",
            colour = "#b30059",
-           size = 3) +
+           size = 4.5) +
   annotate(geom = "curve",
            x = 10, xend = 8.5,
            y = 8180, yend = 11700,
@@ -71,7 +71,7 @@ ggplot(bars, aes(x = decile, y = impact, fill = component)) +
   ) +
   theme(
     plot.title.position = "panel",
-    plot.title = element_text(size = 14, face = "bold", margin = margin(t = 10, b = 10)),
+    plot.title = element_text(size = 18, face = "bold", margin = margin(t = 10, b = 10)),
     legend.position = "inside",
     legend.position.inside = c(0.5, 0.7),
     legend.title = element_blank(),
